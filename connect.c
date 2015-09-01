cd9116bebd58a3d9f0bf31c376a785dc1afe4da9
root <zheng.qiu520@qq.com>
2015-09-01 17:31:39 +0800

    modify

--- connect.c
+++ connect.c
@@ -227,7 +227,7 @@ int do_things(int rfd)
 		qz_printf("i == %d\n",i);
 		f = strstr(html_buff,req_args[i].front);
 		qz_printf("f:%d\n",f-html_buff);
-		if(f <= 0)	{ memcpy(&p_str[i][0],"\'---\'",strlen("\'---\'"));continue; }
+		if(f <= 0)	{ memset(&p_str[i],0,4*1024);memcpy(&p_str[i][0],"\'---\'",strlen("\'---\'"));continue; }
 		if(i < 5)
 		{
 			e = strstr(f+strlen(req_args[i].f_str),req_args[i].end);
